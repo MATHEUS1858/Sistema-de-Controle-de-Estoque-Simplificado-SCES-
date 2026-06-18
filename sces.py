7319eff8044ad2d81f78bd53433b0dfbd118dc7b
MATHEUS VIEIRA <matheus.vieira@portalsesisp.org.br>
2026-06-18 14:12:41 -0300

    adicionar_produto, listar_produtos e buscar_por_id Finalizados

--- sces.py
+++ sces.py
@@ -1,17 +1,33 @@
 rodando = True
 produtos = [
-    [27300, "Rolamento de Rolos Cônicos", 3, "3, 0"],
-    [13783, "Válvula Solenoide", 6, "3, 1"],
-    [89243, "Engrenagem Helicoidal", 5, "3, 2"]
+    ["27300", "Rolamento de Rolos Cônicos", "3", "0"],
+    ["13783", "Válvula Solenoide", "6", "1"],
+    ["89243", "Engrenagem Helicoidal", "5", "2"],
 ]
 
 def adicionar_produto():
-    global produtos
-    produtos = input("Insira seu produto: ")
+    id = input("Digite o ID da peça: ")
+    nomedapeca = input("Digite o nome da peça: ")
+    quantidade = input("Digite a quantidade presente no estoque: ")
+    localizacao = input("Digite a localização da peça: ")
+    novoproduto = [id, nomedapeca, quantidade, localizacao]
+    produtos.append(novoproduto)
+    print("Peça adicionada com sucesso.")
 
 def listar_produtos():
     for i in range(len(produtos)):
-        print(produtos[i][2])
+        print(produtos[i])
+
+def buscar_por_id():
+    buscar = input("Buscar Produto por ID: ")
+    for i in range(len(produtos)):
+        if buscar == produtos[i][0]:
+            print(produtos[i])
+
+def atualizar_estoque():
+    global produtos
+    posicao = input("Digite a posição no eixo y: ")
+    produtos[posicao][2] = input("Digite a quantidade de produtos presentes: ")
 
 while rodando == True:
     print("\nBem vindo ao Sistema de Controle de Estoque Simplificado (SCES). Selecione uma opção:")
@@ -22,8 +38,8 @@ while rodando == True:
     elif opcao == "2":
         listar_produtos()
     elif opcao == "3":
-        listar_produtos()
+        buscar_por_id()
     elif opcao == "4":
-        listar_produtos()
+        atualizar_estoque()
     else:
         rodando = False
